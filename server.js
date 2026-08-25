338511ce4521ab8a1c51de9c5fa7da529c3fd1ff
Chinoo2 <trabajocompu29@gmail.com>
2026-08-25 15:36:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
+
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
@@ -9,6 +10,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -20,30 +22,47 @@ function parseEnvList(env) {
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
 var cors_proxy = require('./lib/cors-anywhere');
+
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
+
+  // Require Origin or X-Requested-With from the client
   requireHeader: ['origin', 'x-requested-with'],
+
   checkRateLimit: checkRateLimit,
+
+  // Headers removed from requests
   removeHeaders: [
     'cookie',
     'cookie2',
+
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
     'via',
     'connect-time',
     'total-route-time',
+
     // Other Heroku added debug headers
     // 'x-forwarded-for',
     // 'x-forwarded-proto',
     // 'x-forwarded-port',
   ],
+
   redirectSameOrigin: true,
+
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+    // Headers that will be sent to the destination server
+    headers: {
+      Origin: 'https://www.izzigo.tv',
+      Referer: 'https://www.izzigo.tv/',
+    },
+
+    // Do not add X-Forwarded-For, etc.
     xfwd: false,
   },
+
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
